f46e3ee95c4f16af700ea5ea6ea946db88265a6f
Ethan Liang <ethanliang97@gmail.com>
2016-01-24 00:25:06 -0500

    Added NFC functionality

--- Xpresscart/app/src/main/java/com/pennapps/xpresscart/LoginActivity.java
+++ Xpresscart/app/src/main/java/com/pennapps/xpresscart/LoginActivity.java
@@ -1,10 +1,13 @@
 package com.pennapps.xpresscart;
 
 import android.app.Dialog;
+import android.app.PendingIntent;
 import android.app.ProgressDialog;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.NfcAdapter;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,6 +18,8 @@ import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import com.reimaginebanking.api.java.NessieClient;
 import com.reimaginebanking.api.java.NessieException;
 import com.reimaginebanking.api.java.NessieResultsListener;
@@ -28,6 +33,7 @@ public class LoginActivity extends AppCompatActivity {
 
     private static Button loginButton;
     private static TextView username, password;
+    private NfcAdapter nfcAdapter;
     private NessieClient nessieClient = NessieClient.getInstance();
 
     @Override
@@ -39,6 +45,17 @@ public class LoginActivity extends AppCompatActivity {
         password = (TextView) findViewById(R.id.password);
         nessieClient.setAPIKey("73f7573087e61516c5fc1304f6b9e985");
 
+
+
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        if(nfcAdapter!=null && nfcAdapter.isEnabled()){
+
+        }
+        else{
+            finish();
+        }
+
+
         loginButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v)
@@ -46,8 +63,9 @@ public class LoginActivity extends AppCompatActivity {
                 try{
                 if (username.getText().toString().equals("abc") && password.getText().toString().equals("abc"))
                 {
-                    Intent i = new Intent(getApplicationContext(),MainActivity.class);
+                    Intent i = new Intent(getApplicationContext(),MerchantActivity.class);
                     startActivity(i);
+                    finish();
                 }
                 }
                 catch (Exception e){
@@ -59,6 +77,30 @@ public class LoginActivity extends AppCompatActivity {
         });
 
     }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        Toast.makeText(this, "Nfc Intent Received!", Toast.LENGTH_LONG).show();
+        super.onNewIntent(intent);
+    }
+
+    @Override
+    protected void onResume() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.addFlags(Intent.FLAG_RECEIVER_REPLACE_PENDING);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+        IntentFilter[] intentFilter = new IntentFilter[]{};
+        nfcAdapter.enableForegroundDispatch(this, pendingIntent, intentFilter, null);
+
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        nfcAdapter.disableForegroundDispatch(this);
+        super.onPause();
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- Xpresscart/app/src/main/java/com/pennapps/xpresscart/MerchantActivity.java
+++ Xpresscart/app/src/main/java/com/pennapps/xpresscart/MerchantActivity.java
@@ -1,50 +1,120 @@
 package com.pennapps.xpresscart;
 
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.tech.Ndef;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.reimaginebanking.api.java.NessieClient;
 import com.reimaginebanking.api.java.NessieException;
 import com.reimaginebanking.api.java.NessieResultsListener;
+import com.reimaginebanking.api.java.models.Customer;
 import com.reimaginebanking.api.java.models.Merchant;
 
+import java.io.ByteArrayOutputStream;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+import java.util.Locale;
 
-public class MerchantActivity extends AppCompatActivity {
+public class MerchantActivity extends Activity {
 
     private NessieClient nessieClient = NessieClient.getInstance();
+    private NfcAdapter nfcAdapter;
+    TextView txtTagContent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_merchant);
+        final TextView merchantName = (TextView) findViewById(R.id.merchant_text);
         nessieClient.setAPIKey("73f7573087e61516c5fc1304f6b9e985");
-        nessieClient.getMerchants(new NessieResultsListener() {
-            @Override
-            public void onSuccess(Object result, NessieException e) {
-                if (e == null) {
-                    //There is no error, do whatever you need here.
-                    // Cast the result object to the type that you are requesting and you are good to go
-                    ArrayList<Merchant> merchants = (ArrayList<Merchant>) result;
-                    for (Merchant merc : merchants) {
-                        Log.i("Merchant", merc.getName());
-                    }
-                } else {
-                    //There was an error. Handle it here
-                    Log.e("Error", e.toString());
-                }
-            }
-        });
+
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        txtTagContent = (TextView) findViewById(R.id.cart_id);
+
+//        nessieClient.getMerchant("56241a13de4bf40b171127fe", new NessieResultsListener() {
+//
+//            @Override
+//            public void onSuccess(Object result, NessieException e) {
+//                if(e == null){
+//                    //There is no error, do whatever you need here.
+//                    // Cast the result object to the type that you are requesting and you are good to go
+//                   merchantName.setText(Merchant);
+//                } else {
+//                    //There was an error. Handle it here
+//                    Log.e("Error", e.toString())
+//                }
+//            }
+//        });
+
+    }
+
+
+    private void readTextFromMessage(NdefMessage ndefMessage) {
+
+        NdefRecord[] ndefRecords = ndefMessage.getRecords();
+
+        if(ndefRecords != null && ndefRecords.length>0){
+
+            NdefRecord ndefRecord = ndefRecords[0];
+
+            String tagContent = getTextFromNdefRecord(ndefRecord);
+
+            txtTagContent.setText(tagContent);
+
+        }else
+        {
+            Toast.makeText(this, "No NDEF records found!", Toast.LENGTH_SHORT).show();
+        }
 
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_merchant, menu);
-        return true;
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        Parcelable[] parcelables = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+
+        if(parcelables != null && parcelables.length > 0)
+        {
+            readTextFromMessage((NdefMessage) parcelables[0]);
+            Intent i = new Intent(getApplicationContext(),MainActivity.class);
+            startActivity(i);
+            finish();
+
+        }else{
+            Toast.makeText(this, "No NDEF messages found!", Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        Intent intent = new Intent(this, MerchantActivity.class);
+        intent.addFlags(Intent.FLAG_RECEIVER_REPLACE_PENDING);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+        IntentFilter[] intentFilter = new IntentFilter[]{};
+        nfcAdapter.enableForegroundDispatch(this, pendingIntent, intentFilter, null);
+
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        nfcAdapter.disableForegroundDispatch(this);
+        super.onPause();
     }
 
     @Override
@@ -61,4 +131,45 @@ public class MerchantActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private NdefRecord createTextRecord(String content) {
+        try {
+            byte[] language;
+            language = Locale.getDefault().getLanguage().getBytes("UTF-8");
+
+            final byte[] text = content.getBytes("UTF-8");
+            final int languageSize = language.length;
+            final int textLength = text.length;
+            final ByteArrayOutputStream payload = new ByteArrayOutputStream(1 + languageSize + textLength);
+
+            payload.write((byte) (languageSize & 0x1F));
+            payload.write(language, 0, languageSize);
+            payload.write(text, 0, textLength);
+
+            return new NdefRecord(NdefRecord.TNF_WELL_KNOWN, NdefRecord.RTD_TEXT, new byte[0], payload.toByteArray());
+
+        } catch (UnsupportedEncodingException e) {
+            Log.e("createTextRecord", e.getMessage());
+        }
+        return null;
+    }
+
+
+
+    public String getTextFromNdefRecord(NdefRecord ndefRecord)
+    {
+        String tagContent = null;
+        try {
+            byte[] payload = ndefRecord.getPayload();
+            String textEncoding = ((payload[0] & 128) == 0) ? "UTF-8" : "UTF-16";
+            int languageSize = payload[0] & 0063;
+            tagContent = new String(payload, languageSize + 1,
+                    payload.length - languageSize - 1, textEncoding);
+        } catch (UnsupportedEncodingException e) {
+            Log.e("getTextFromNdefRecord", e.getMessage(), e);
+        }
+        return tagContent;
+    }
+
+
 }
